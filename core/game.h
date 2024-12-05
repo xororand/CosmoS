aa6691821a3dd23a067d8d9cb6eafa09d7e8f1d2
xororand <159540042+xororand@users.noreply.github.com>
2024-12-05 13:04:20 +0300

    box2d

---
+++ core/game.h
@@ -0,0 +1,11 @@
+#pragma once
+#include "defines.h"
+#include "includes.h"
+
+class game
+{
+public:
+	game();
+	~game();
+};
+
---
+++ version.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#define COSMOS_VER_MAJOR 0
+#define COSMOS_VER_MINOR 1
+#define COSMOS_VER_PATCH 1
